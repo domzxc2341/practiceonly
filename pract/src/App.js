5b52841fb52997956f78e097a381a4e8709b7ac9
domzxc2341 <dominiquesimbillo01@gmail.com>
2022-08-20 21:26:29 +0800

    ye
    ye

--- pract/src/App.js
+++ pract/src/App.js
@@ -6,20 +6,20 @@ function App() {
 
   {/*   this is xl 1280 width pxp */}
       <div class=" bg-gradient-to-r from-indigo-900 to-teal-900 flex
-      xs:w-[400px] h-[640px]
+      xs:w-[360px] h-[640px]
       xl:h-screen 
       ">
     
-      <p class="text-[7vh] font-mono text-violet-600 relative font-bold
-      xs:text-[3vh] xs:top-[20vh] xs:left-[5vh] xs:
+      <p class="text-[7vh] font-mono text-violet-600 relative font-bold 
+      xs:text-[3vh] xs:top-[15vh] xs:left-[12vh] xs:whitespace-nowrap 
       ">Hello I'm Dominique</p>
 
       <p class="relative font-mono text-violet-600 font-bold
-      xs:text-[3vh] xs:top-[30vh] xs:right-[15vh]
+      xs:text-[3vh] xs:top-[30vh] xs:right-[15vh] xs:whitespace-nowrap 
       ">And I'm Web Developer </p>
 
       <img src={img1} class="relative inline 
-      xs:h-[13vh] xs:right-[25vh] xs:top-[45vh]
+      xs:h-[13vh] xs:right-[30vh] xs:top-[45vh]
       sm:left-[105vh] sm:h-[45vh]
       xl:left-[95vh] xl:bottom-[20vh] 
        "/>
