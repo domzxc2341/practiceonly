272bcc32e1038677eb318a4d38eb62d4606254fc
domzxc2341 <dominiquesimbillo01@gmail.com>
2022-08-20 17:37:56 +0800

    ye
    ye

--- pract/src/App.js
+++ pract/src/App.js
@@ -1,3 +1,5 @@
+import img1 from "./Photo_2.jpg"
+
 function App() {
   return (
     <>
@@ -5,7 +7,7 @@ function App() {
         <div class="relative top-[30vh] left-[15vh]  inline ">
       <h1 class="text-[7vh] font-mono text-violet-600">Hello I'm Dominique</h1>
       <p class="relative top-[2vh] text-[5vh] font-mono text-violet-600">And I'm Web Developer </p>
-      
+      <img src={img1} />
       </div>
   </div>
       <h1 class="text-4xl relative top-20 left-20 inline">Welcome</h1>
