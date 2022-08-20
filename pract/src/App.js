b8da499dfedc3a61a90b0fff83cee104f84bda6f
domzxc2341 <dominiquesimbillo01@gmail.com>
2022-08-20 15:25:56 +0800

    ye
    ye

--- pract/src/App.js
+++ pract/src/App.js
@@ -2,7 +2,12 @@ function App() {
   return (
     <>
       <div class=" h-screen bg-gradient-to-r from-indigo-900 to-teal-900 flex">
-      <h1 class="text-4xl relative top-20 inline ">Welcome to mobile legends</h1></div>
+        <div class="relative top-[30vh] left-[15vh]  inline ">
+      <h1 class="text-[7vh] font-mono text-violet-600">Hello I'm Dominique</h1>
+      <p class="relative top-[2vh] text-[5vh] font-mono text-violet-600">And I'm Web Developer </p>
+      
+      </div>
+  </div>
       <h1 class="text-4xl relative top-20 left-20 inline">Welcome</h1>
     </>
   );
