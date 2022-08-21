9c89ba665f9bdf9597b56052f41cbdfeda5586f8
domzxc2341 <dominiquesimbillo01@gmail.com>
2022-08-21 14:41:34 +0800

    ye
    ye

--- pract/src/App.js
+++ pract/src/App.js
@@ -1,31 +1,34 @@
 import img1 from "./Photo_2.png"
+import './App.css'
 
 function App() {
   return (
     <>
+        <div class="bdy">
 
-  {/*   this is xl 1280 width pxp */}
       <div class=" bg-gradient-to-r from-indigo-900 to-teal-900 flex
-      xs:w-[360px] h-[640px]
-      xl:h-screen 
+      xs:w-[360px] xs:h-[640px] 
+      sm:w-[640px]
+      
       ">
     
-      <p class="text-[7vh] font-mono text-violet-600 relative font-bold 
-      xs:text-[3vh] xs:top-[15vh] xs:left-[12vh] xs:whitespace-nowrap 
+      <p class="text-[7vh] font-mono text-violet-600 relative font-bold inline
+      xs:text-[4vh] xs:top-[27vh] xs:left-[8vh] xs:whitespace-nowrap 
       ">Hello I'm Dominique</p>
 
-      <p class="relative font-mono text-violet-600 font-bold
-      xs:text-[3vh] xs:top-[30vh] xs:right-[15vh] xs:whitespace-nowrap 
+      <p class="relative font-mono text-violet-600 font-bold inline
+      xs:text-[4vh] xs:top-[35vh] xs:right-[35vh] xs:whitespace-nowrap 
       ">And I'm Web Developer </p>
 
-      <img src={img1} class="relative inline 
-      xs:h-[13vh] xs:right-[30vh] xs:top-[45vh]
+      <img src={img1} class="relative
+      xs:h-[30vh] xs:right-[83vh] xs:top-[50vh]
       sm:left-[105vh] sm:h-[45vh]
-      xl:left-[95vh] xl:bottom-[20vh] 
+      xl:left-[15vh] xl:top-[40vh]
        "/>
   
   </div>
       <h1 class="text-4xl relative top-20 left-20 inline">Welcome</h1>
+      </div>
     </>
   );
 }
